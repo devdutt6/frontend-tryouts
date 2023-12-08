d2cae57c29b59c715a8351a7b27660e4c509e2f7
Devduttsinh <devduttchudasama1339@gmail.com>
2023-12-08 19:11:29 +0530

    added svelte

---
+++ svelte/src/routes/card/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Card from "../../components/Card.svelte";
+	import NameList from "../../components/NameList.svelte";
+  import Display from "../../components/Display.svelte";
+  import Increment from "../../components/Increment.svelte";
+	import Decrement from "../../components/Decrement.svelte";
+	import Reset from "../../components/Reset.svelte";
+	import Time from "../../components/Time.svelte";
+</script>
+
+<Card />
+<Card>Hello from content</Card>
+<Card><h2>Hello from content</h2></Card>
+<Card>
+  <div slot='card'>CARD COMPO</div>
+  <div slot='header'><h3>HEADER COMPO</h3></div>
+  <div slot='content'>Content</div>
+  <div slot='footer'>FOOTER COMPO</div>
+</Card>
+
+<NameList>
+  <h3 slot="hero" let:fname let:lname>{fname} {lname}</h3>
+</NameList>
+<NameList>
+  <h3 slot="hero" let:fname let:lname>{lname} {fname}</h3>
+</NameList>
+<NameList>
+  <h3 slot="hero" let:lname>{lname}</h3>
+</NameList>
+
+<!-- Stores -->
+<Display />
+<Increment />
+<Decrement />
+<Reset />
+
+<!-- Readable store -->
+<Time />
---
+++ svelte/src/routes/product/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  const products = [
+    {
+      id: 1,
+      name: 'jeans'
+    },
+    {
+      id: 2,
+      name: 'football'
+    },
+    {
+      id: 3,
+      name: 'airpods'
+    },
+  ]
+</script>
+
+
+<h2>Product list</h2>
+{#each products as product (product.id)}
+  <h3><a href={`/product/${product.id}`}>{product.name}</a></h3>
+{/each}
+
+<a href="/">Home</a>
